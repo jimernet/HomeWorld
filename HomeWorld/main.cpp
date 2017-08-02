afcc1009153c07977928b8f12c3c356746286801
Rodrigo Jimenez <MAC_RJP@macbook-pro-de-rodrigo.local>
2017-08-01 23:31:46 -0400

    Initial Commit

---
+++ HomeWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomeWorld
+//
+//  Created by Rodrigo Jimenez on 01-08-17.
+//  Copyright © 2017 Rodrigo Jimenez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
